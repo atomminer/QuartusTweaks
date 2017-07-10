9b516b900e37e6ac2ed0e7a1c9426664c055e6dd
AtomMiner <atom@atomminer.com>
2017-07-10 03:56:52 +0300

    New script to Setup Quartus Lite PATH and add USB Blaster rules

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 QuartusTweaks
+
+usbblaster.sh - this script will add default Quartus bin folder to environment PATH and create libudev rules for USB Blaster
---
+++ usbblaster.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+#title           :usbblaster.sh
+#description     :This script will add Quartus folder to environment PATH and create udev rules for Altera USB Blaster.
+#author          :www.atomminer.com
+#version         :0.1
+
+# kill jtagd
+echo "Stopping JTAG daemon..."
+killall jtagd
+
+# add Quartus bin folder to PATH
+echo "Addind Quartus to environment PATH..."
+if [ -d "$HOME/altera_lite/15.0/quartus/bin" ] ; then
+    echo "   Using Quartus 15.0"
+    echo "PATH=\"$PATH:$HOME/altera_lite/15.0/quartus/bin\"" >> ~/.bashrc
+    PATH="$PATH:$HOME/altera_lite/15.0/quartus/bin"
+fi
+if [ -d "$HOME/altera_lite/15.1/quartus/bin" ] ; then
+    echo "   Using Quartus 15.1"
+    echo "PATH=\"$PATH:$HOME/altera_lite/15.1/quartus/bin\"" >> ~/.bashrc
+    PATH="$PATH:$HOME/altera_lite/15.1/quartus/bin"
+fi
+if [ -d "$HOME/altera_lite/16.0/quartus/bin" ] ; then
+    echo "   Using Quartus 16.0"
+    echo "PATH=\"$PATH:$HOME/altera_lite/16.0/quartus/bin\"" >> ~/.bashrc
+    PATH="$PATH:$HOME/altera_lite/16.0/quartus/bin"
+fi
+if [ -d "$HOME/altera_lite/16.1/quartus/bin" ] ; then
+    echo "   Using Quartus 16.1"
+    echo "PATH=\"$PATH:$HOME/altera_lite/16.1/quartus/bin\"" >> ~/.bashrc
+    PATH="$PATH:$HOME/altera_lite/16.1/quartus/bin"
+fi
+if [ -d "$HOME/altera_lite/17.0/quartus/bin" ] ; then
+    echo "   Using Quartus 17.0"
+    echo "PATH=\"$PATH:$HOME/altera_lite/17.0/quartus/bin\"" >> ~/.bashrc
+    PATH="$PATH:$HOME/altera_lite/17.0/quartus/bin"
+fi
+
+source ~/.bashrc
+
+# add USB Blaster rules to udev
+echo "Setting up USB Blaster rules..."
+sudo touch /etc/udev/rules.d/usbblaster.rules
+echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"09fb\", ATTR{idProduct}==\"6001\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/usbblaster.rules -a
+echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"09fb\", ATTR{idProduct}==\"6002\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/usbblaster.rules -a
+echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"09fb\", ATTR{idProduct}==\"6003\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/usbblaster.rules -a
+echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"09fb\", ATTR{idProduct}==\"6010\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/usbblaster.rules -a
+echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"09fb\", ATTR{idProduct}==\"6810\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/usbblaster.rules -a
+
+echo "Reloading libudev rules..."
+sudo udevadm control --reload
+
+echo "Quartus Prime Lite should be all set. Script created by http://www.atomminer.com"
+
